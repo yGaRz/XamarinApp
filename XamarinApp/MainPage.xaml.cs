b5c6a121217b9851b88124ba5ed0545536b3d53e
yGaR <ygar@rambler.ru>
2023-05-24 09:13:11 +0300

    Добавьте файлы проекта.

---
+++ XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int t = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            t++;
+            lbl1.Text = t.ToString();
+        }
+    }
+}
---
+++ XamarinApp/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
